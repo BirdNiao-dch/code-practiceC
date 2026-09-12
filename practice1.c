7f004709145e774bc78892e71d6a50d9dace8dc5
Birdch <41536919@qq.com>
2026-09-12 16:48:03 +0800

    Add files via upload

---
+++ practice1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	//提示输入一个ASCII码值，然后打印输入的字符
+	/*int input;
+	printf("enter a value of char int ASCII:");
+	scanf("%d", &input);
+	printf("%c\n",input);*/
+
+	//把年龄转换成天数并显示天数
+	/*int age;
+	scanf("%d\n", &age);
+	int days;
+
+	days = age * 365;
+	printf("your age is %d and it is %d days\n", age,days);
+
+	return 0;*/
+
+	//输入一个字符打印对应的整数
+	/*char ch;
+	printf("please enter a character:\n");
+	scanf("%c", &ch);
+	printf("the code for% c is% d.\n", ch, ch);
+	return 0;*/
+}
