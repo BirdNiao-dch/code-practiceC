170de5deb1adfa9c4210d58812d733c06d40eade
Birdch <41536919@qq.com>
2026-09-14 23:16:23 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int sum = 0;
+//
+//	scanf("%d", &n);
+//	sum = n * (n + 1) / 2;
+//
+//	printf("%d\n", sum);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main(void)
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	if (a > b)
+//		printf("The max number is %d.",a);
+//	else
+//		printf("The max number is %d.",b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main(void)
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//
+//	if (a >= 90)
+//		printf("A\n");
+//	else if (a >= 80 && a < 90)
+//		printf("B\n");
+//	else if (a >= 70 && a < 80)
+//		printf("C\n");
+//	else if (a >= 60 && a < 70)
+//		printf("D\n");
+//	else if (0<=a&&a< 60)
+//		printf("E\n");
+//	return 0;
+//
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	while (scanf("%d", &a) != EOF)
+//	printf("Hello%d\n", a);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//		while (scanf("%d%d", &a,&b)!=EOF) {
+//			printf("%d\n", a + b);
+//	}
+//		return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	double a = 0.0;
+//	double b = 0.0;
+//	double avg = 0.0;
+//	scanf("%lf %lf", &a, &b);
+//	avg = (a + b) / 2.0;
+//	printf("Average = %.2f",avg);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int sum = 0;
+//	while (scanf("%d", &a) != EOF) {
+//		sum += a;
+//	}
+//		printf("%d\n",sum);
+//		return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	double R;
+//	double s;
+//	scanf("%lf",&R);
+//	s = 3.14159 * R * R;
+//	printf("%.3f", s);
+//	return 0;
+//}
