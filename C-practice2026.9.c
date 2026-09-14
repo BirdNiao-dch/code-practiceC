6ee556f0c20b364340efd3972c06874728170976
Birdch <41536919@qq.com>
2026-09-14 13:45:36 +0800

    Add files via upload

---
+++ C-practice2026.9.c
@@ -0,0 +1,11 @@
+﻿生成开始于 13:33...
+1>------ 已启动生成: 项目: practice2026.9, 配置: Debug x64 ------
+1>  practice1.c
+1>E:\code\practice2026.9\practice1.c(41,12): warning C4267: “=”: 从“size_t”转换到“int”，可能丢失数据
+1>E:\code\practice2026.9\practice1.c(42,18): warning C4244: “=”: 从“double”转换到“float”，可能丢失数据
+1>E:\code\practice2026.9\practice1.c(45,48): warning C4474: printf: 格式字符串中传递的参数太多
+1>      E:\code\practice2026.9\practice1.c(45,48):
+1>      占位符和其参数预计 0 可变参数，但提供的却是 1 参数
+1>  practice2026.9.vcxproj -> E:\code\practice2026.9\x64\Debug\practice2026.9.exe
+========== 生成: 1 成功，0 失败，0 最新，0 已跳过 ==========
+========== 生成 于 13:33 完成，耗时 00.615 秒 ==========
